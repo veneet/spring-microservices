7f907194d0c5dd6f81d3669dd28acdb2d73af7bb
VkPandey <VkPandey@HIBAML134348.ad.harman.com>
2020-01-27 12:16:44 +0530

    spring-cloud-config-server

---
+++ spring-cloud-config-server/src/main/java/com/java/microservices/SpringCloudConfigServerApp.java
@@ -0,0 +1,13 @@
+package com.java.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApp.class, args);
+	}
+
+}
